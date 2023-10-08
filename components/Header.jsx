ede214f8869ee32bd24927a915cf2f611e82ddb8
mamidelibas <mamidlbs@gmail.com>
2023-10-08 15:03:55 +0300

    rjs

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,53 +1,61 @@
-import Avatar from "@mui/material/Avatar";
-import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
+import TextField from "@mui/material/TextField";
 import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
-import { deepOrange, deepPurple } from "@mui/material/colors";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Avatar from "@mui/material/Avatar";
+import { deepPurple } from "@mui/material/colors";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
 
 export default function Header() {
   return (
-    <>
-      <header className="flex justify-around py-5 px-5 w-full items-center mt-3 border-b-2 border-gray-200">
-        <div className="flex items-center gap-1.5">
-          <CameraAltOutlinedIcon
-            className="border rounded-full p-1 bg-rose-700"
-            fontSize="large"
-            style={{ fill: "#fff" }}
-          />
-          <h1 className="text-xl font-bold text-rose-700">forToday</h1>
-        </div>
+    <header className="flex justify-between px-7 py-6 border-b-2 border-gray-200">
+      <Image src={Logo} height={55} />
 
-        <div>
-          <input
-            type="text"
-            placeholder="Search now..."
-            className="rounded-full border pl-2"
-            style={{ width: "500px" }}
-          />
-        </div>
+      <TextField
+        id="outlined-basic"
+        placeholder="Search now..."
+        sx={{
+          width: "750px",
+          borderRadius: "500px",
+        }}
+        InputProps={{
+          style: {
+            borderRadius: "1000px",
+          },
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
 
-        <div className="flex items-center gap-12">
-          <div>
-            <HeadsetMicIcon
-              className="border rounded-full p-0.5"
-              fontSize="large"
-            />
-          </div>
-          <div>
-            <NotificationsNoneIcon
-              className="border rounded-full p-0.5"
-              fontSize="large"
-            />
-          </div>
-          <div className="flex items-center gap-4">
-            <span className="text-xl font-bold">Vinc.rangga</span>
-
-            <Avatar sx={{ bgcolor: deepOrange[500] }}>MD</Avatar>
-            <KeyboardArrowDownOutlinedIcon />
-          </div>
-        </div>
-      </header>
-    </>
+      <div className="flex items-center gap-3">
+        <HeadsetMicIcon
+          sx={{
+            fontSize: 42,
+            color: "#374151",
+            border: "1px solid #e5e7eb",
+            padding: "5px",
+            borderRadius: "50%",
+          }}
+        />
+        <NotificationsIcon
+          sx={{
+            fontSize: 42,
+            color: "#374151",
+            border: "1px solid #e5e7eb",
+            padding: "5px",
+            borderRadius: "50%",
+          }}
+        />
+        <span className="text-xl font-bold ml-7">Vinc.ragga</span>
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>VR</Avatar>
+        <ExpandMoreIcon sx={{ fontSize: 28 }} />
+      </div>
+    </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-
-import Header
- from "@/components/Header"
+import ContentArea from "@/components/ContentArea";
+import Header from "@/components/Header";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Home() {
   return (
-   <div>
     <div>
-      <Header/>
+      <div>
+        <Header />
+      </div>
+      <div className="flex">
+        <div className="w-1/4 bg-gray-100">
+          <LeftSidebar />
+        </div>
+        <div className="w-2/4 bg-gray-400">
+          <ContentArea />
+        </div>
+        <div className="w-1/4 bg-gray-800">
+          <RightSidebar />
+        </div>
+      </div>
     </div>
-    <div className="">
-      {/* content area buraya gelecek*/}
-    </div>
-   </div>
-  )
+  );
 }
